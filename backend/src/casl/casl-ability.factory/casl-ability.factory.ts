b83b7fa904617cb4cd423cb4454f6258540a4e9d
EduardoLizarazu <jelo2021@gmail.com>
2025-02-20 08:29:59 -0400

    Update casl-ability.factory.ts

--- backend/src/casl/casl-ability.factory/casl-ability.factory.ts
+++ backend/src/casl/casl-ability.factory/casl-ability.factory.ts
@@ -1,27 +1,34 @@
 import {
-  Ability,
   AbilityBuilder,
-  AbilityClass,
+  createMongoAbility,
   ExtractSubjectType,
   InferSubjects,
+  MongoAbility,
+  MongoQuery,
 } from '@casl/ability';
 import { Injectable } from '@nestjs/common';
 import { Action } from '../../auth/enums/actions.enum';
-import { ClientRole } from '../../auth/enums/role.enum';
-import { userHasAnyRole } from '../../auth/helpers/auth.helpers';
 import { User } from '../../users/entities/user.entity';
+import { userHasAnyRole } from 'src/auth/helpers/auth.helpers';
+import { ClientRole } from 'src/auth/enums/role.enum';
 
 type Subjects = InferSubjects<typeof User> | 'all';
 
-export type AppAbility = Ability<[Action, Subjects]>;
+type PossibleAbilities = [Action, Subjects];
+type Conditions = MongoQuery;
+
+export type AppAbility = MongoAbility<PossibleAbilities, Conditions>;
 
 @Injectable()
 export class CaslAbilityFactory {
   createForUser(user: User) {
-    const { can, cannot, build } = new AbilityBuilder<
-      Ability<[Action, Subjects]>
-    >(Ability as AbilityClass<AppAbility>);
+    const { can, build } = new AbilityBuilder(
+      createMongoAbility<PossibleAbilities, Conditions>,
+    );
 
+    console.log('CaslAbilityFactory user: ', user);
+
+    // Admin can do everything
     if (userHasAnyRole(user, [ClientRole.Admin])) {
       can(Action.Manage, 'all'); // read-write access to everything
     } else {
@@ -29,6 +36,19 @@ export class CaslAbilityFactory {
     }
 
     can(Action.UpdateOwn, User, { id: user.id });
+
+    // // For each role the user has, add each permission
+    // user.roles?.forEach((role) => {
+    //   role.permissions?.forEach((permission: Permission) => {
+    //     // Here we assume permission.action and permission.subject exist,
+    //     // and permission.conditions is optional.
+    //     can(
+    //       permission.name as Action,
+    //       permission.subject as ExtractSubjectType<Subjects>,
+    //       permission.conditions || {},
+    //     );
+    //   });
+    // });
 
     return build({
       // Read https://casl.js.org/v6/en/guide/subject-type-detection#use-classes-as-subject-types for details
