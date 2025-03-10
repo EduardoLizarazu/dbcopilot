11559fb78047f1e750bc09a1eb1b407628710a27
EduardoLizarazu <jelo2021@gmail.com>
2025-03-10 11:28:03 -0400

    Update page.tsx

--- frontend-nextjs/src/app/auth/roles/[roleId]/page.tsx
+++ frontend-nextjs/src/app/auth/roles/[roleId]/page.tsx
@@ -1,5 +1,21 @@
 "use client";
-import { CircularProgress } from "@mui/material";
+import { GetPermissionDataModel } from "@/data/model/index.data.model";
+import {
+  Button,
+  CircularProgress,
+  Container,
+  Divider,
+  Paper,
+  Stack,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TextField,
+  Typography,
+} from "@mui/material";
 import React from "react";
 
 interface EditRolePageProps {
@@ -11,6 +27,38 @@ interface EditRolePageProps {
 export default function EditRolePage({ params }: EditRolePageProps) {
   const [loading, setLoading] = React.useState<boolean>(true);
   const [roleId, setRoleId] = React.useState<number>(0);
+  const [roleName, setRoleName] = React.useState<string>("");
+  const [permissions, setPermissions] = React.useState<
+    GetPermissionDataModel[]
+  >([]);
+  const [selectedPermissions, setSelectedPermissions] = React.useState<
+    GetPermissionDataModel[]
+  >([]);
+
+  async function handleAddPermission(permission: GetPermissionDataModel) {
+    // add permission to selected permission
+    setSelectedPermissions([...selectedPermissions, permission]);
+    // remove permission from permissions
+    setPermissions(permissions.filter((perm) => perm.id !== permission.id));
+  }
+
+  async function handleRemovePermission(permission: GetPermissionDataModel) {
+    // remove permission from selected permission
+    setSelectedPermissions(
+      selectedPermissions.filter((perm) => perm.id !== permission.id)
+    );
+    // add permission to permissions
+    setPermissions([...permissions, permission]);
+  }
+
+  async function handleEditRole() {
+    const editRoleDto: EditRoleDataModel = {
+      id: roleId,
+      name: roleName,
+      permissions: selectedPermissions,
+    };
+    await EditRole(editRoleDto);
+  }
 
   React.useEffect(() => {
     (async () => {
@@ -25,8 +73,93 @@ export default function EditRolePage({ params }: EditRolePageProps) {
   }
 
   return (
-    <div>
-      <h1>Edit Role with id: {roleId}</h1>
-    </div>
+    <Container sx={{ my: 4 }}>
+      <Typography variant="h4">Edit role</Typography>
+
+      <TextField
+        id="name-textfield"
+        label="Name"
+        variant="standard"
+        style={{ width: "100%" }}
+        value={roleName}
+        onChange={(e) => setRoleName(e.target.value)}
+      />
+      <Divider className="my-8" />
+      <Typography variant="h6">Permissions Selected: </Typography>
+      <TableContainer component={Paper} className="my-4">
+        <Table sx={{ minWidth: 650 }} size="small" aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="left">Name</TableCell>
+              <TableCell align="left">Description</TableCell>
+              <TableCell align="left">Action</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {selectedPermissions.map((perm) => (
+              <TableRow key={perm.id}>
+                <TableCell align="left">{perm.name}</TableCell>
+                <TableCell align="left">{perm.description}</TableCell>
+                <TableCell align="left">
+                  <Button
+                    variant="contained"
+                    onClick={() => handleRemovePermission(perm)}
+                    color="error"
+                  >
+                    Remove
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+
+      <Divider className="my-8" />
+      <Typography variant="h6">Permissions: </Typography>
+      <TableContainer component={Paper} className="my-4">
+        <Table sx={{ minWidth: 650 }} size="small" aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="left">Name</TableCell>
+              <TableCell align="left">Description</TableCell>
+              <TableCell align="left">Action</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {permissions.map((perm) => (
+              <TableRow key={perm.id}>
+                <TableCell align="left">{perm.name}</TableCell>
+                <TableCell align="left">{perm.description}</TableCell>
+                <TableCell align="left">
+                  <Button
+                    variant="contained"
+                    onClick={() => handleAddPermission(perm)}
+                    color="info"
+                  >
+                    Add
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+
+      <Divider className="my-8" />
+      <Stack direction="row" spacing={2} sx={{ my: 2 }}>
+        {/* Error color */}
+        <Button
+          variant="contained"
+          color="error"
+          onClick={() => history.back()}
+        >
+          Cancel
+        </Button>
+        <Button variant="contained" color="primary" onClick={handleEditRole}>
+          Edit Role
+        </Button>
+      </Stack>
+    </Container>
   );
 }
