d639784a50baa87433359aae10894a4b8eae52a9
EduardoLizarazu <jelo2021@gmail.com>
2025-03-10 18:31:58 -0400

    user case to be resolve

--- frontend-nextjs/src/domain/entities/index.domain.entity.ts
+++ frontend-nextjs/src/domain/entities/index.domain.entity.ts
@@ -1,4 +1,6 @@
 import { RoleEntity } from "./role.domain.entity";
 import { PermissionEntity } from "./permission.domain.entity";
+import { UserEntity } from "./user.domain.entity";
 
-export { RoleEntity, PermissionEntity };
+
+export { RoleEntity, PermissionEntity, UserEntity };
--- frontend-nextjs/src/useCase/user/createUser.usecase.ts
+++ frontend-nextjs/src/useCase/user/createUser.usecase.ts
@@ -1,17 +1,30 @@
+import { CreateUserDataModel, GetPermissionDataModel, GetRolesDataModel } from "@/data/model/index.data.model";
 import { UserRepository } from "@/data/repo/index.data.repo";
+import { UserEntity } from "@/domain/entities/index.domain.entity";
+import { EmailValueObject, IdValueObject, PhoneValueObject } from "@/domain/valueObject/index.domain.valueObject";
 
-interface CreateUserInput {
+interface CreateUserInput extends CreateUserDataModel{};
 
-}
-
-interface CreateUserOutput {
-  
-}
+interface CreateUserOutput {}
 
 export class CreateUserUseCase {
   constructor(private readonly userRepository: UserRepository) {}
 
   async execute(input: CreateUserInput): Promise<CreateUserOutput> {
+    const idVo = new IdValueObject(0);
+    const phoneVo = new PhoneValueObject(input.phone);
+    const emailVo = new EmailValueObject(input.email);
+    const userEntity: UserEntity = new UserEntity(
+      idVo,
+      input.firstName,
+      emailVo,
+      input.lastName,
+      phoneVo.value,
+      input.roles,
+      input.permissions,
+      input.isActive
+    );
+
     const user = await this.userRepository.createUser(input);
     return {
       user: user,
