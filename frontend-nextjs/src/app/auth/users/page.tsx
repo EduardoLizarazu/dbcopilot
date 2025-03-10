05a580cc73b20fa5336785c5ea5ddd495912700a
EduardoLizarazu <jelo2021@gmail.com>
2025-03-10 15:42:16 -0400

    Remove unnecessary folders and files

--- frontend-nextjs/src/app/auth/users/page.tsx
+++ frontend-nextjs/src/app/auth/users/page.tsx
@@ -1,11 +1,10 @@
+"use client";
 import React from "react";
-import { UserTable } from "@/app/auth/users/table.user";
 
 const UsersPage: React.FC = () => {
   return (
     <div>
       <h1>Users</h1>
-      <UserTable />
     </div>
   );
 };
