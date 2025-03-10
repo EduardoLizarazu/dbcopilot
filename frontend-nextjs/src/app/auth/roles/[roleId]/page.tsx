c79c840580f32e8c6d1364874a569ade0d399778
EduardoLizarazu <jelo2021@gmail.com>
2025-03-10 11:19:24 -0400

    get the id form url on edit role

--- frontend-nextjs/src/app/auth/roles/[roleId]/page.tsx
+++ frontend-nextjs/src/app/auth/roles/[roleId]/page.tsx
@@ -1,20 +1,32 @@
 "use client";
+import { CircularProgress } from "@mui/material";
 import React from "react";
 
 interface EditRolePageProps {
-  params: {
+  params: Promise<{
     roleId: string;
-  };
+  }>;
 }
 
 export default function EditRolePage({ params }: EditRolePageProps) {
+  const [loading, setLoading] = React.useState<boolean>(true);
+  const [roleId, setRoleId] = React.useState<number>(0);
+
   React.useEffect(() => {
-    console.log("Edit Role with id: ", params.roleId);
-  }, [params.roleId]);
+    (async () => {
+      const { roleId } = await params;
+      setRoleId(parseInt(roleId));
+    })();
+    setLoading(false);
+  }, [params]);
+
+  if (loading) {
+    return <CircularProgress />;
+  }
 
   return (
     <div>
-      <h1>Edit Role with id: {params.roleId}</h1>
+      <h1>Edit Role with id: {roleId}</h1>
     </div>
   );
 }
