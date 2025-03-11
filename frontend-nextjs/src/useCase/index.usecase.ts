470fadf99769b4ad928c4d5318a5e9c81091aa72
EduardoLizarazu <jelo2021@gmail.com>
2025-03-11 11:18:23 -0400

    Remove unnecessary imports files from use case layer

--- frontend-nextjs/src/useCase/index.usecase.ts
+++ frontend-nextjs/src/useCase/index.usecase.ts
@@ -1,39 +1,20 @@
-import {
-  CreateRoleUseCase,
-  ReadRolesUseCase,
-  ReadRoleByIdUseCase,
-  EditRoleUseCase,
-  RemoveRoleUseCase,
-} from "./role/role.usecase";
-import {
-  CreatePermissionUseCase,
-  ReadPermissionUseCase,
-  ReadPermissionByIdUseCase,
-  UpdatePermissionUseCase,
-  DeletePermissionByIdUseCase,
-} from "./permission/permission.usecase";
-import {
-  CreateUserUseCase,
-  DeleteUserUseCase,
-  ReadUserByIdUseCase,
-  ReadUsersUseCase,
-  UpdateUserUseCase,
-} from "./user/user.usecase";
+// PERMISSION USE CASES
+export { CreatePermissionUseCase } from "./permission/createPermission.usecase";
+export { ReadPermissionUseCase } from "./permission/readPermission.usecase";
+export { ReadPermissionByIdUseCase } from "./permission/readPermissionById.usecase";
+export { UpdatePermissionUseCase } from "./permission/updatePermission.usecase";
+export { DeletePermissionByIdUseCase } from "./permission/deletePermissionById.usecase";
 
-export {
-  CreateRoleUseCase,
-  ReadRolesUseCase,
-  ReadRoleByIdUseCase,
-  EditRoleUseCase,
-  RemoveRoleUseCase,
-  CreatePermissionUseCase,
-  ReadPermissionUseCase,
-  ReadPermissionByIdUseCase,
-  UpdatePermissionUseCase,
-  DeletePermissionByIdUseCase,
-  CreateUserUseCase,
-  ReadUsersUseCase,
-  ReadUserByIdUseCase,
-  UpdateUserUseCase,
-  DeleteUserUseCase,
-};
+// ROLE USE CASES
+export { CreateRoleUseCase } from "./role/createRole.usecase";
+export { ReadRolesUseCase } from "./role/readRoles.usecase";
+export { ReadRoleByIdUseCase } from "./role/readRoleById.usecase";
+export { EditRoleUseCase } from "./role/editRole.usecase";
+export { RemoveRoleUseCase } from "./role/removeRole.usecase";
+
+// USER USE CASES
+export { CreateUserUseCase } from "./user/createUser.usecase";
+export { ReadUsersUseCase } from "./user/readUsers.usecase";
+export { ReadUserByIdUseCase } from "./user/readUserById.usecase";
+export { UpdateUserUseCase } from "./user/updateUser.usecase";
+export { DeleteUserUseCase } from "./user/deleteUser.usecase";
