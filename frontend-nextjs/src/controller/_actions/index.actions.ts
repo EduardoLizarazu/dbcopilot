ec16dc9cd28fd395917fe4b343684427f1e91698
EduardoLizarazu <jelo2021@gmail.com>
2025-03-11 11:25:20 -0400

    Simplify export on controller later

--- frontend-nextjs/src/controller/_actions/index.actions.ts
+++ frontend-nextjs/src/controller/_actions/index.actions.ts
@@ -1,39 +1,21 @@
-import {
+export {
   GetRoles,
   CreateRole,
   GetRoleById,
   UpdateRole,
   DeleteRole,
 } from "../_actions/role/role.action";
-import {
+export {
   GetPermissions,
   GetPermissionById,
   UpdatePermission,
   DeletePermissionById,
   CreatePermission,
 } from "./permission/permission.action";
-import { 
-  CreateUser, 
-  DeleteUser, 
-  GetUserById, 
-  GetUsers, 
-  UpdateUser 
-} from "./user/user.action";
-
 export {
-  CreateRole,
-  GetRoles,
-  GetRoleById,
-  UpdateRole,
-  DeleteRole,
-  CreatePermission,
-  GetPermissions,
-  GetPermissionById,
-  UpdatePermission,
-  DeletePermissionById,
   CreateUser,
-  GetUsers,
+  DeleteUser,
   GetUserById,
+  GetUsers,
   UpdateUser,
-  DeleteUser
-};
+} from "./user/user.action";
