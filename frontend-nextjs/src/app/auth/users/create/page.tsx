e6360269bddd5293b176dde58167bd49ac737ddd
EduardoLizarazu <jelo2021@gmail.com>
2025-03-11 15:23:18 -0400

    add direct permissions tap

--- frontend-nextjs/src/app/auth/users/create/page.tsx
+++ frontend-nextjs/src/app/auth/users/create/page.tsx
@@ -1,20 +1,35 @@
 "use client";
-import { CreateUser } from "@/controller/_actions/index.actions";
 import {
+  CreateUser,
+  GetPermissions,
+  GetRoles,
+} from "@/controller/_actions/index.actions";
+import React from "react";
+import {
+  Box,
   Button,
   CircularProgress,
   Container,
   Divider,
   Link,
+  Paper,
   Stack,
+  Tab,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
   TextField,
   Typography,
 } from "@mui/material";
-import React from "react";
+import { TabContext, TabList, TabPanel } from "@mui/lab";
 
 export default function CreateUserPage() {
   // HOOK
   const [loading, setLoading] = React.useState<boolean>(true);
+  const [value, setValue] = React.useState("1");
   const [username, setUsername] = React.useState<string>("");
   const [email, setEmail] = React.useState<string>("");
   const [password, setPassword] = React.useState<string>("");
@@ -32,11 +47,31 @@ export default function CreateUserPage() {
 
   React.useEffect(() => {
     (async () => {
+      if (value === "1") setRoles(await GetRoles());
+      if (value === "2") setPermissions(await GetPermissions());
       setLoading(false);
     })();
-  }, []);
+  }, [value]);
 
   // HANDLERS
+  async function handleChange(event: React.SyntheticEvent, newValue: string) {
+    setValue(newValue);
+  }
+
+  function handleAddPermission(permission) {
+    // add permission to selected permission
+    setSelectedPermissions([...selectedPermissions, permission]);
+    // remove permission from permissions
+    setPermissions(permissions.filter((perm) => perm.id !== permission.id));
+  }
+  function handleRemovePermission(permission) {
+    // remove permission from selected permission
+    setSelectedPermissions(
+      selectedPermissions.filter((perm) => perm.id !== permission.id)
+    );
+    // add permission to permissions
+    setPermissions([...permissions, permission]);
+  }
 
   async function handleCreate() {
     const createUserDto = {
@@ -127,6 +162,87 @@ export default function CreateUserPage() {
       </Stack>
 
       <Divider className="my-8" />
+
+      <Box sx={{ width: "100%", typography: "body1" }}>
+        <TabContext value={value}>
+          <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+            <TabList onChange={handleChange} aria-label="authorization tabs">
+              <Tab label="Roles" value="1" />
+              <Tab label="Direct Permission" value="2" />
+            </TabList>
+          </Box>
+          <TabPanel value="1">Roles</TabPanel>
+          <TabPanel value="2">
+            <Typography variant="h6">Permissions Selected: </Typography>
+            <TableContainer component={Paper} className="my-4">
+              <Table
+                sx={{ minWidth: 650 }}
+                size="small"
+                aria-label="simple table"
+              >
+                <TableHead>
+                  <TableRow>
+                    <TableCell align="left">Name</TableCell>
+                    <TableCell align="left">Description</TableCell>
+                    <TableCell align="left">Action</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {selectedPermissions.map((perm) => (
+                    <TableRow key={perm.id}>
+                      <TableCell align="left">{perm.name}</TableCell>
+                      <TableCell align="left">{perm.description}</TableCell>
+                      <TableCell align="left">
+                        <Button
+                          variant="contained"
+                          onClick={() => handleRemovePermission(perm)}
+                          color="error"
+                        >
+                          Remove
+                        </Button>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+
+            <Divider className="my-8" />
+            <Typography variant="h6">Permissions: </Typography>
+            <TableContainer component={Paper} className="my-4">
+              <Table
+                sx={{ minWidth: 650 }}
+                size="small"
+                aria-label="simple table"
+              >
+                <TableHead>
+                  <TableRow>
+                    <TableCell align="left">Name</TableCell>
+                    <TableCell align="left">Description</TableCell>
+                    <TableCell align="left">Action</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {permissions.map((perm) => (
+                    <TableRow key={perm.id}>
+                      <TableCell align="left">{perm.name}</TableCell>
+                      <TableCell align="left">{perm.description}</TableCell>
+                      <TableCell align="left">
+                        <Button
+                          variant="contained"
+                          onClick={() => handleAddPermission(perm)}
+                        >
+                          Add
+                        </Button>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </TabPanel>
+        </TabContext>
+      </Box>
     </Container>
   );
 }
