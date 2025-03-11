2c56ffc295893adadbba3920ef1f6a1abb882f33
EduardoLizarazu <jelo2021@gmail.com>
2025-03-11 10:13:52 -0400

    Update user.domain.entity.ts

--- frontend-nextjs/src/domain/entities/user.domain.entity.ts
+++ frontend-nextjs/src/domain/entities/user.domain.entity.ts
@@ -1,33 +1,38 @@
 import { IdValueObject } from "../valueObject/id.domain.valueObject";
-import { EmailValueObject, PhoneValueObject } from "../valueObject/index.domain.valueObject";
+import {
+  EmailValueObject,
+  PhoneValueObject,
+} from "../valueObject/index.domain.valueObject";
+
+interface UserProps {
+  id: IdValueObject;
+  username: string;
+  email: EmailValueObject;
+  password: string;
+  firstName: string;
+  lastName: string;
+  phone: PhoneValueObject;
+  createdAt: Date;
+}
 
 export class UserEntity {
-    private readonly _id: IdValueObject;
-    private readonly _username: string;
-    private readonly _email: EmailValueObject;
-    private readonly _password: string;
-    private readonly _firstName: string;
-    private readonly _lastName: string;
-    private readonly _phone: PhoneValueObject;
-    private readonly _createdAt: Date;
+  private readonly _id: IdValueObject;
+  private readonly _username: string;
+  private readonly _email: EmailValueObject;
+  private readonly _password: string;
+  private readonly _firstName: string;
+  private readonly _lastName: string;
+  private readonly _phone: PhoneValueObject;
+  private readonly _createdAt: Date;
 
-    constructor(
-        id: IdValueObject,
-        username: string,
-        email: EmailValueObject,
-        password: string,
-        firstName: string,
-        lastName: string,
-        phone: PhoneValueObject,
-        createdAt: Date
-    ) {
-        this._id = id;
-        this._username = username;
-        this._email = email;
-        this._password = password;
-        this._firstName = firstName;
-        this._lastName = lastName;
-        this._phone = phone;
-        this._createdAt = createdAt;
-    }
+  constructor(props: UserProps) {
+    this._id = props.id;
+    this._username = props.username;
+    this._email = props.email;
+    this._password = props.password;
+    this._firstName = props.firstName;
+    this._lastName = props.lastName;
+    this._phone = props.phone;
+    this._createdAt = props.createdAt;
+  }
 }
