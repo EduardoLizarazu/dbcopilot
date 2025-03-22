a1329747f79ccf6abdc8ac3d41a0a65ca9e58341
EduardoLizarazu <jelo2021@gmail.com>
2025-03-22 10:49:42 -0400

    👷 (user-auth): Data Models - Get roles for user
    on this commit:
    - Add omit permission, because of failure on over write the "permission" inherited, making two separated permission on the object.

--- frontend-nextjs/src/data/model/role/getRolesForUser.data.model.ts
+++ frontend-nextjs/src/data/model/role/getRolesForUser.data.model.ts
@@ -1,5 +1,6 @@
 import { GetPermissionDataModel, GetRolesDataModel } from "../index.data.model";
 
-export interface GetRolesForUserDataModel extends GetRolesDataModel {
+export interface GetRolesForUserDataModel
+  extends Omit<GetRolesDataModel, "permissions"> {
   permissions: (GetPermissionDataModel & { isActive: boolean })[];
 }
