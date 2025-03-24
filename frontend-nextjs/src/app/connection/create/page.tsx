afe29d05913d801c4e9537cf6dde5695e40e7ca1
EduardoLizarazu <jelo2021@gmail.com>
2025-03-24 16:54:21 -0400

    👷 (connection): Create Connection Page
    Still working on the page.

--- frontend-nextjs/src/app/connection/create/page.tsx
+++ frontend-nextjs/src/app/connection/create/page.tsx
@@ -1,9 +1,41 @@
-import { Container, Typography } from "@mui/material";
+import React from "react";
+import { CircularProgress, Container, Typography } from "@mui/material";
 
 export default function CreateConnectionPage() {
+  // USE STATE
+  const [loading, setLoading] = React.useState<boolean>(true);
+  const [connName, setConnName] = React.useState<string>("");
+  const [description, setDescription] = React.useState<string>("");
+  const [databaseType, setDatabaseType] = React.useState<string>("");
+  const [host, setHost] = React.useState<string>("");
+  const [port, setPort] = React.useState<number>(0);
+  const [databaseName, setDatabaseName] = React.useState<string>("");
+  const [username, setUsername] = React.useState<string>("");
+  const [password, setPassword] = React.useState<string>("");
+  
+  // USE EFFECT
+  React.useEffect(() => {
+    (async () => {
+      setLoading(true);
+
+      // Fetch data here
+
+      setLoading(false);
+    })();
+  }, []);
+
+  // HANDLERS
+
+
+  // RENDERS
+  if (loading) {
+    return <CircularProgress />;
+  }
+
   return (
     <Container>
       <Typography variant="h4">Create Connection</Typography>
+
     </Container>
   );
 }
