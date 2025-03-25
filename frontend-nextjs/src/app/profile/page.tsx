89ce79ce00f9c1d64c55398574bb6e9e6995bd22
EduardoLizarazu <jelo2021@gmail.com>
2025-03-25 08:42:11 -0400

    Create page.tsx

---
+++ frontend-nextjs/src/app/profile/page.tsx
@@ -0,0 +1,9 @@
+import { Container, Typography } from "@mui/material";
+import React from "react";
+export default function ProfilePage() {
+  return (
+    <Container>
+      <Typography variant="h4">Profile</Typography>
+    </Container>
+  );
+}
