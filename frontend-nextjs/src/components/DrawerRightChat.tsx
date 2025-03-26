a63c2601896036df750e48c14a0a0f3000735e80
EduardoLizarazu <jelo2021@gmail.com>
2025-03-26 14:42:22 -0400

    Update DrawerRightChat.tsx

--- frontend-nextjs/src/components/DrawerRightChat.tsx
+++ frontend-nextjs/src/components/DrawerRightChat.tsx
@@ -2,14 +2,9 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 import Drawer from "@mui/material/Drawer";
 import Button from "@mui/material/Button";
-import List from "@mui/material/List";
 import Divider from "@mui/material/Divider";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import MailIcon from "@mui/icons-material/Mail";
+import { SchemaList } from "./schemaList";
+import { ChatStoryList } from "./chatStoryList";
 
 export function DrawerRightChat() {
   const [open, setOpen] = React.useState(false);
@@ -19,43 +14,22 @@ export function DrawerRightChat() {
   };
 
   const DrawerList = (
-    <Box
-      sx={{ width: 400, marginTop: 10 }}
-      role="presentation"
-      onClick={toggleDrawer(false)}
-    >
-      <List>
-        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
+    <Box sx={{ width: 400, marginTop: 10 }} role="presentation">
+      <SchemaList />
       <Divider />
-      <List>
-        {["All mail", "Trash", "Spam"].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
+      <ChatStoryList />
     </Box>
   );
 
   return (
     <div>
       <Button onClick={toggleDrawer(true)}>Open drawer</Button>
-      <Drawer open={open} anchor="right" onClose={toggleDrawer(false)}>
+      <Drawer
+        open={open}
+        anchor="right"
+        variant="temporary"
+        onClose={toggleDrawer(false)}
+      >
         {DrawerList}
       </Drawer>
     </div>
