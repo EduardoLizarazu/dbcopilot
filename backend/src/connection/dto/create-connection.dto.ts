9549eb45807750b4f41010837bce9b11f5d010ba
EduardoLizarazu <jelo2021@gmail.com>
2025-03-31 16:06:53 -0400

    Swagger

--- backend/src/connection/dto/create-connection.dto.ts
+++ backend/src/connection/dto/create-connection.dto.ts
@@ -1,33 +1,43 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsNotEmpty, IsNumber } from 'class-validator';
 
 
 export class CreateConnectionDto {
+    @ApiProperty()
     @IsString()
     @IsNotEmpty()
     name: string;
 
+    @ApiProperty()
     @IsString()
     description?: string;
 
+    @ApiProperty()
     @IsString()
     @IsNotEmpty()
     dbType: string;
 
+    @ApiProperty()
     @IsString()
     @IsNotEmpty()
+    @ApiProperty()
     dbName: string;
 
+    @ApiProperty()
     @IsString()
     @IsNotEmpty()
     dbHost: string;
 
+    @ApiProperty()
     @IsNumber()
     dbPort: number;
 
+    @ApiProperty()
     @IsString()
     @IsNotEmpty()
     dbUsername: string;
 
+    @ApiProperty()
     @IsString()
     dbPassword?: string;
 }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { config } from 'dotenv';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // swagger
+  const config = new DocumentBuilder()
+    .setTitle('API documentation')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .addTag('API')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory, {
+    jsonDocumentUrl: 'swagger/json',
+  });
+
   await app.listen(process.env.PORT ?? 3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
