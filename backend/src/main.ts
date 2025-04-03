061bbf5c128b4d1b000135cb89160f2c9b6fa740
EduardoLizarazu <84466380+EduardoLizarazu@users.noreply.github.com>
2025-04-03 15:18:57 -0400

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
     jsonDocumentUrl: 'swagger/json',
   });
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.SERVER_PORT ?? 3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
