5d0f297379909b12d44b6b02e98e9e37778e224f
EduardoLizarazu <84466380+EduardoLizarazu@users.noreply.github.com>
2025-04-05 15:26:08 -0400

    Import schema entities module

--- backend/src/schema/schema_relation/schema_relation.module.ts
+++ backend/src/schema/schema_relation/schema_relation.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SchemaRelationService } from './schema_relation.service';
 import { SchemaRelationController } from './schema_relation.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SchemaRelation } from './entities/schema_relation.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([SchemaRelation])],
   controllers: [SchemaRelationController],
   providers: [SchemaRelationService],
 })
--- backend/src/schema/schema_table/schema_table.module.ts
+++ backend/src/schema/schema_table/schema_table.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SchemaTableService } from './schema_table.service';
 import { SchemaTableController } from './schema_table.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SchemaTable } from './entities/schema_table.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([SchemaTable])], // Add your entities here if needed
   controllers: [SchemaTableController],
   providers: [SchemaTableService],
 })
