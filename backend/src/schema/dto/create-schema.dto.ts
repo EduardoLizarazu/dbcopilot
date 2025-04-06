721e8258acb8b16499d0aa3ee43c789f455a960f
EduardoLizarazu <84466380+EduardoLizarazu@users.noreply.github.com>
2025-04-06 12:00:17 -0400

    Create schema dto from test

--- backend/src/schema/dto/create-schema.dto.ts
+++ backend/src/schema/dto/create-schema.dto.ts
@@ -1,1 +1,8 @@
-export class CreateSchemaDto {}
+export class CreateSchemaDto {
+    table_name: string;
+    column_name: string;
+    data_type: string;
+    key_type: string;
+    reference_table: string;
+    reference_column: string;
+}
