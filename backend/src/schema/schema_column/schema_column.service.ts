2ea39131f77735dad71f7691a95fa735d69ef80e
EduardoLizarazu <84466380+EduardoLizarazu@users.noreply.github.com>
2025-04-09 17:59:32 -0400

    Schema column service
    Get the column with the relation base on table's id

--- backend/src/schema/schema_column/schema_column.service.ts
+++ backend/src/schema/schema_column/schema_column.service.ts
@@ -3,13 +3,14 @@ import { CreateSchemaColumnDto } from './dto/create-schema_column.dto';
 import { UpdateSchemaColumnDto } from './dto/update-schema_column.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { SchemaColumn } from './entities/schema_column.entity';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 
 @Injectable()
 export class SchemaColumnService {
   constructor(
     @InjectRepository(SchemaColumn)
     private schemaColumnRepository: Repository<SchemaColumn>,
+    private dataSource: DataSource,
   ) {}
   async create(createSchemaColumnDto: CreateSchemaColumnDto) {
     try {
@@ -28,7 +29,7 @@ export class SchemaColumnService {
   async findAll() {
     try {
       return await this.schemaColumnRepository.find({
-        relations: ['schemaColumnKeys', 'schemaTable', 'schemaRelationFather', 'schemaRelationChild'],
+        relations: ['schemaRelation'],
       });
     } catch (error) {
       throw new Error(`Error fetching schema columns: ${error.message}`);
@@ -37,16 +38,38 @@ export class SchemaColumnService {
 
   async findOne(id: number) {
     try {
-      const schemaColumn = await this.schemaColumnRepository.findOne({
-        where: { id },
-        relations: ['schemaColumnKeys', 'schemaTable', 'schemaRelationFather', 'schemaRelationChild'],
-      });
-      if (!schemaColumn) {
-        throw new Error(`Schema column with ID ${id} not found`);
-      }
+      const schemaColumn = await this.schemaColumnRepository.findOne(
+        { where: { id }});
       return schemaColumn;
     } catch (error) {
       throw new Error(`Error fetching schema column: ${error.message}`);
+    }
+  }
+
+  async findByTableId(tableId: number) {
+    try {
+      /**
+       * -- Select all schema_columns (id) belonging to a schema_table (id) with the relations
+        select schema_column.id, schema_column."technicalName",
+        schema_relation."columnIdChild", schema_relation."columnIdFather"
+        from schema_column
+        left join schema_relation on schema_column.id = schema_relation."columnIdChild"
+        where schema_column."schemaTableId" = 222;
+      */
+      // with datasource
+      const schemaColumns = await this.dataSource.query(
+        `SELECT schema_column.id, schema_column."technicalName", schema_relation."columnIdChild", schema_relation."columnIdFather"
+        FROM schema_column
+        LEFT JOIN schema_relation ON schema_column.id = schema_relation."columnIdChild"
+        WHERE schema_column."schemaTableId" = $1`, [tableId],
+      );
+      if (!schemaColumns) {
+        throw new Error(`Schema columns for table ID ${tableId} not found`);
+      }
+      return schemaColumns;
+    }
+    catch (error) {
+      throw new Error(`Error fetching schema columns by table ID: ${error.message}`);
     }
   }
 
