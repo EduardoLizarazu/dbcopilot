64503a7a5ce0670b1079714a9d99ba3bcff1edde
EduardoLizarazu <84466380+EduardoLizarazu@users.noreply.github.com>
2025-04-09 14:46:55 -0400

    Fetch the connection id on frontend

--- frontend-nextjs/src/app/connection/[connectionId]/schema/page.tsx
+++ frontend-nextjs/src/app/connection/[connectionId]/schema/page.tsx
@@ -1,8 +1,11 @@
-"use client";
 import { Container, Typography } from "@mui/material";
 import { SchemaTableList } from "@/components/schema/schemaTableList";
 
-export default function SchemaPage() {
+export default async function SchemaPage({ params }: { params: { connectionId: string } }) {
+  const { connectionId } = await params;
+
+  console.log("SchemaPage connectionId", connectionId);
+  
   return (
     <Container>
       <Typography variant="h4">Schema</Typography>
