3dc97e0129feba63c0db0fceb5feef2d0edbf6e3
EduardoLizarazu <84466380+EduardoLizarazu@users.noreply.github.com>
2025-04-13 20:08:52 -0400

    removing additional columns

--- backend/src/schema/schema_column/schema_column.service.ts
+++ backend/src/schema/schema_column/schema_column.service.ts
@@ -52,19 +52,27 @@ export class SchemaColumnService {
     try {
       /**
        * -- Select all schema_columns (id) belonging to a schema_table (id) with the relations
-        select schema_column.id, schema_column."technicalName",
-        schema_relation."columnIdChild", schema_relation."columnIdFather"
+        select schema_column.id, schema_column."technicalName", schema_column.alias, schema_column."dataType",
+        schema_relation."columnIdChild", schema_relation."columnIdFather", schema_relation."isStatic",
+        schema_column_key_column."is_static",
+        schema_column_key."type"
         from schema_column
         left join schema_relation on schema_column.id = schema_relation."columnIdChild"
-        where schema_column."schemaTableId" = 222;
+        left join schema_column_key_column on schema_column.id = schema_column_key_column.id_schema_column
+        left join schema_column_key on schema_column_key_column.id_column_key = schema_column_key.id
+        where schema_column."schemaTableId" = 249;
       */
       // with datasource
       const schemaColumns = await this.dataSource.query(
-        `SELECT schema_column.id, schema_column."technicalName", schema_column.alias, schema_column."dataType",
-        schema_relation."columnIdChild", schema_relation."columnIdFather", schema_relation.description
-        FROM schema_column
-        LEFT JOIN schema_relation ON schema_column.id = schema_relation."columnIdChild"
-        WHERE schema_column."schemaTableId" = $1`,
+        `select schema_column.id, schema_column."technicalName", schema_column.alias, schema_column."dataType",
+        schema_relation."columnIdChild", schema_relation."columnIdFather", schema_relation."isStatic" as schema_relation_is_static,
+        schema_column_key_column."is_static" as key_column_is_static,
+        schema_column_key."type" as key_type 
+        from schema_column
+        left join schema_relation on schema_column.id = schema_relation."columnIdChild"
+        left join schema_column_key_column on schema_column.id = schema_column_key_column.id_schema_column
+        left join schema_column_key on schema_column_key_column.id_column_key = schema_column_key.id
+        where schema_column."schemaTableId" = $1`,
         [tableId],
       );
       if (!schemaColumns) {
