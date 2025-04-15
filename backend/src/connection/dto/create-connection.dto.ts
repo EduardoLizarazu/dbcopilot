5ef93e746fd60fc883268d3ac0636f1053e3070d
EduardoLizarazu <84466380+EduardoLizarazu@users.noreply.github.com>
2025-04-15 15:37:11 -0400

    add to connection dto

--- backend/src/connection/dto/create-connection.dto.ts
+++ backend/src/connection/dto/create-connection.dto.ts
@@ -1,42 +1,45 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString, IsNotEmpty, IsNumber } from 'class-validator';
-
+import { IsString, IsNotEmpty, IsNumber, IsBoolean } from 'class-validator';
 
 export class CreateConnectionDto {
-    @ApiProperty()
-    @IsString()
-    @IsNotEmpty()
-    name: string;
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  name: string;
 
-    @ApiProperty()
-    @IsString()
-    description?: string;
+  @ApiProperty()
+  @IsString()
+  description?: string;
 
-    @ApiProperty()
-    @IsString()
-    @IsNotEmpty()
-    dbTypeId: number;
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  dbTypeId: number;
 
-    @ApiProperty()
-    @IsString()
-    @IsNotEmpty()
-    dbName: string;
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  dbName: string;
 
-    @ApiProperty()
-    @IsString()
-    @IsNotEmpty()
-    dbHost: string;
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  dbHost: string;
 
-    @ApiProperty()
-    @IsNumber()
-    dbPort: number;
+  @ApiProperty()
+  @IsNumber()
+  dbPort: number;
 
-    @ApiProperty()
-    @IsString()
-    @IsNotEmpty()
-    dbUsername: string;
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  dbUsername: string;
 
-    @ApiProperty()
-    @IsString()
-    dbPassword?: string;
+  @ApiProperty()
+  @IsString()
+  dbPassword?: string;
+
+  @ApiProperty()
+  @IsBoolean()
+  is_connected: boolean;
 }
