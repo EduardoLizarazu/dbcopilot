66418fdc8abe148b51778477c0bda905589dd5a9
EduardoLizarazu <84466380+EduardoLizarazu@users.noreply.github.com>
2025-04-15 15:35:18 -0400

    add is connected and format

--- backend/src/connection/entities/connection.entity.ts
+++ backend/src/connection/entities/connection.entity.ts
@@ -1,39 +1,48 @@
-import { Databasetype } from "src/databasetype/entities/databasetype.entity";
-import { SchemaTable } from "src/schema/schema_table/entities/schema_table.entity";
-import { Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Databasetype } from 'src/databasetype/entities/databasetype.entity';
+import { SchemaTable } from 'src/schema/schema_table/entities/schema_table.entity';
+import {
+  Column,
+  Entity,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity()
 export class Connection {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column({unique: true})
-    name: string;
+  @Column({ unique: true })
+  name: string;
 
-    @Column({ nullable: true })
-    description?: string;
+  @Column({ nullable: true })
+  description?: string;
 
-    @Column()
-    dbName: string;
+  @Column()
+  dbName: string;
 
-    @Column()
-    dbHost: string;
+  @Column()
+  dbHost: string;
 
-    @Column()
-    dbPort: number;
+  @Column()
+  dbPort: number;
 
-    @Column()
-    dbUsername: string;
+  @Column()
+  dbUsername: string;
 
-    @Column({ select: false })
-    dbPassword?: string;
-    
-    // Connection only has one and only one database type
-    @ManyToOne(() => Databasetype, (databasetype) => databasetype.connections)
-    databasetype: Databasetype;
+  @Column({ select: false })
+  dbPassword?: string;
 
-    @OneToMany(() => SchemaTable,
-        (schemaTable) => schemaTable.connection, { onDelete: "CASCADE" })
-    schemaTables: SchemaTable[]; 
+  @Column({ nullable: true })
+  is_connected: boolean;
+
+  // Connection only has one and only one database type
+  @ManyToOne(() => Databasetype, (databasetype) => databasetype.connections)
+  databasetype: Databasetype;
+
+  @OneToMany(() => SchemaTable, (schemaTable) => schemaTable.connection, {
+    onDelete: 'CASCADE',
+  })
+  schemaTables: SchemaTable[];
 }
-
