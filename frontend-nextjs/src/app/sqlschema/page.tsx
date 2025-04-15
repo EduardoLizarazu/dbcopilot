179f8c9f80c94b4cab87f97e98ae99f59c50e68d
EduardoLizarazu <84466380+EduardoLizarazu@users.noreply.github.com>
2025-04-15 17:31:27 -0400

    create sql schema table action component

--- frontend-nextjs/src/app/sqlschema/page.tsx
+++ frontend-nextjs/src/app/sqlschema/page.tsx
@@ -1,62 +1,77 @@
 "use client";
-import { deleteSqlSchemaAction, readAllSqlSchemaAction } from "@/controller/_actions/index.actions";
-import { Button, CircularProgress, Container, Link, Paper, Stack, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField, Typography } from "@mui/material";
+import { SqlSchemaTableAction } from "@/components/sqlschema/sqlschema_table_action";
+import {
+  deleteSqlSchemaAction,
+  readAllSqlSchemaAction,
+} from "@/controller/_actions/index.actions";
+import {
+  Button,
+  CircularProgress,
+  Container,
+  Link,
+  Paper,
+  Stack,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TextField,
+  Typography,
+} from "@mui/material";
 import React, { useState } from "react";
 
-
 interface ListInterface {
-    id: number;
-    name: string;
-    type: string;
-    query: string;
+  id: number;
+  name: string;
+  type: string;
+  query: string;
 }
 
 export default function Page() {
-    // USE STATE
-    const [loading, setLoading] = useState(false);
-    const [sqlSchema, setSqlSchema] = React.useState<ListInterface[]>([]);
+  // USE STATE
+  const [loading, setLoading] = useState(false);
+  const [sqlSchema, setSqlSchema] = React.useState<ListInterface[]>([]);
 
-    // USE EFFECT
-    React.useEffect(() => {
-        (async () => {
-            setLoading(true);
-            const response = await readAllSqlSchemaAction();
-            setSqlSchema(
-                response.map((item: ListInterface) => {
-                    return {
-                        id: item.id || 0,
-                        name: item.name || "",
-                        type: item.type || "",
-                        query: item.query || "",
-                    };
-                })
-            );
-            setLoading(false);
-            console.log(response);
-            
-        })();
-    }, []);
+  // USE EFFECT
+  React.useEffect(() => {
+    (async () => {
+      setLoading(true);
+      const response = await readAllSqlSchemaAction();
+      setSqlSchema(
+        response.map((item: ListInterface) => {
+          return {
+            id: item.id || 0,
+            name: item.name || "",
+            type: item.type || "",
+            query: item.query || "",
+          };
+        })
+      );
+      setLoading(false);
+      console.log(response);
+    })();
+  }, []);
 
-
-    // HANDLER
-    async function handleRemove(id: number): Promise<void> {
-      try {
-        await deleteSqlSchemaAction(id);  
-        setSqlSchema((prev) => prev.filter((item) => item.id !== id));
-      } catch (error) {
-        console.error("Error removing SQL schema:", error);
-      }
+  // HANDLER
+  async function handleRemove(id: number): Promise<void> {
+    try {
+      await deleteSqlSchemaAction(id);
+      setSqlSchema((prev) => prev.filter((item) => item.id !== id));
+    } catch (error) {
+      console.error("Error removing SQL schema:", error);
     }
+  }
 
+  // RENDER
+  if (loading) {
+    return <CircularProgress />;
+  }
 
-    // RENDER
-    if(loading) {
-        return <CircularProgress />;
-    }
-
-    return (
-        <Container>
-      <Typography variant="h4">Connections</Typography>
+  return (
+    <Container>
+      <Typography variant="h4">Sql Connection extract schema</Typography>
       <Link href="/sqlschema/create">
         <Button variant="contained" color="primary">
           Create
@@ -66,36 +81,34 @@ export default function Page() {
         <Table sx={{ minWidth: 650 }} size="small" aria-label="simple table">
           <TableHead>
             <TableRow>
-              <TableCell align="left">Name</TableCell>
-              <TableCell align="left">Type</TableCell>
-              <TableCell align="left">SQL</TableCell>
-              <TableCell align="left">Action</TableCell>
+              <TableCell align="center">Name</TableCell>
+              <TableCell align="center">Type</TableCell>
+              <TableCell align="center">SQL</TableCell>
+              <TableCell align="center" />
             </TableRow>
           </TableHead>
           <TableBody>
             {sqlSchema.map((item) => (
               <TableRow key={item.id}>
-                <TableCell align="left">{item.name}</TableCell>
-                <TableCell align="left">{item.type}</TableCell>
-                <TableCell align="left">{
+                <TableCell align="center">{item.name}</TableCell>
+                <TableCell align="center">{item.type}</TableCell>
+                <TableCell align="center">
+                  {
                     // if it is more than 50 characters, show only 50 characters and add ... at the end
-                    item.query.length > 50 ? item.query.substring(0, 30) + "..." : item.query
-                    }</TableCell>
-                <TableCell align="left">
-                  <Stack direction="row" spacing={2}>
-                    <Link href={`/sqlschema/${item.id}`}>
-                      <Button variant="contained" color="info">
-                        Edit
-                      </Button>
-                    </Link>
-                    <Button
-                      variant="contained"
-                      onClick={() => handleRemove(item.id)}
-                      color="error"
-                    >
-                      Remove
-                    </Button>
-                  </Stack>
+                    item.query.length > 50
+                      ? item.query.substring(0, 30) + "..."
+                      : item.query
+                  }
+                </TableCell>
+                <TableCell align="center">
+                  <SqlSchemaTableAction
+                    handleEditBtn={function (): void {
+                      throw new Error("Function not implemented.");
+                    }}
+                    handleDeleteBtn={function (): void {
+                      throw new Error("Function not implemented.");
+                    }}
+                  />
                 </TableCell>
               </TableRow>
             ))}
@@ -103,5 +116,5 @@ export default function Page() {
         </Table>
       </TableContainer>
     </Container>
-    );
+  );
 }
