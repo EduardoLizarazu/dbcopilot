7912a9b51b0774e2246576b036d0ee3859d712ff
EduardoLizarazu <84466380+EduardoLizarazu@users.noreply.github.com>
2025-04-16 00:58:32 -0400

    create sql schema

--- frontend-nextjs/src/app/sqlschema/create/page.tsx
+++ frontend-nextjs/src/app/sqlschema/create/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { CreateSqlSchemaAction } from "@/controller/_actions/index.actions";
+import { CreateSqlSchemaActionInput } from "@/controller/_actions/sqlschema/interface/sqlschema_create.interface";
 import {
   Autocomplete,
   Button,
@@ -9,30 +10,46 @@ import {
   TextField,
   Typography,
 } from "@mui/material";
-import React, { useState } from "react";
+import React from "react";
 export default function Page() {
   // USE STATE
-  const [loading, setLoading] = useState(false);
-  const [name, setName] = React.useState("");
-  const [type, setType] = React.useState("");
-  const [sqlSchema, setSqlSchema] = React.useState("");
+  const [loading, setLoading] = React.useState(false);
+  const [data, setData] = React.useState<CreateSqlSchemaActionInput>({
+    name: "",
+    type: "",
+    query: "",
+  });
+  const [type, setType] = React.useState<string>("");
 
   // USE EFFECT
   React.useEffect(() => {}, []);
 
   // HANDLER
 
-  async function handleSubmit(): Promise<void> {
+  async function handleCreate(): Promise<void> {
     try {
       await CreateSqlSchemaAction({
-        name: name,
+        name: data.name,
         type: type,
-        query: sqlSchema,
+        query: data.query,
       });
     } catch (error) {
       console.error("Error creating SQL schema:", error);
       alert("Error creating SQL schema. Please try again.");
     }
+  }
+
+  function handleTextField(e: React.ChangeEvent<HTMLInputElement>) {
+    const { name, value } = e.target;
+    setData((prev) => ({ ...prev, [name]: value }));
+  }
+
+  function resetData() {
+    setData({
+      name: "",
+      type: "",
+      query: "",
+    });
   }
 
   // RENDER
@@ -50,12 +67,13 @@ export default function Page() {
   return (
     <Container>
       <Stack spacing={2}>
-        <Typography variant="h4">SQL Schema</Typography>
+        <Typography variant="h4">Create SQL Schema</Typography>
         <TextField
           label="Name"
+          name="name"
           variant="outlined"
           value={name}
-          onChange={(e) => setName(e.target.value)}
+          onChange={handleTextField}
           fullWidth
         />
         <Autocomplete
@@ -71,24 +89,17 @@ export default function Page() {
         />
         <TextField
           label="SQL Schema"
+          name="query"
           variant="outlined"
-          value={sqlSchema}
-          onChange={(e) => setSqlSchema(e.target.value)}
+          value={data.query}
+          onChange={handleTextField}
           fullWidth
           multiline
           rows={4}
         />
         <Stack direction="row" spacing={2} justifyContent="flex-end">
-          <Button onClick={() => handleSubmit()}>Submit</Button>
-          <Button
-            onClick={() => {
-              setName("");
-              setType("");
-              setSqlSchema("");
-            }}
-          >
-            Clear
-          </Button>
+          <Button onClick={handleCreate}>Create</Button>
+          <Button onClick={resetData}>Reset</Button>
         </Stack>
       </Stack>
     </Container>
