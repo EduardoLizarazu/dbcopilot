d3086268705ab58caea4b7784448e40b8e2ac39a
EduardoLizarazu <jelo2021@gmail.com>
2025-04-21 16:11:49 -0400

    create context with feedback

--- frontend-nextjs/src/app/layout.tsx
+++ frontend-nextjs/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import BasicLayoutNav from "@/components/BasicLayoutNav";
 import "./globals.css";
+import { SchemaContextProvider } from "@/contexts/schema.context";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
---
+++ frontend-nextjs/src/contexts/feedback.context.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { FeedbackSnackBar } from "@/components/shared/feedbackSnackBar";
+import React from "react";
+
+type TSeverity = "success" | "error" | "warning" | "info" | null;
+
+type TFeedbackContext = {
+    feedback : {
+        message: string;
+        severity: TSeverity
+    };
+    setFeedback: React.Dispatch< 
+        React.SetStateAction<{
+            message: string;
+            severity: TSeverity;
+        }>
+    >;
+}
+
+
+const FeedbackContext = React.createContext<TFeedbackContext | null>(null);
+
+
+
+export function useFeedbackContext() {
+    const context = React.useContext(FeedbackContext);
+    if (!context) {
+        throw new Error("useFeedbackContext must be used within a FeedbackProvider");
+    }
+    return context;
+}
+
+
+export function FeedbackContextProvider({
+    children,
+}: {
+    children: React.ReactNode;
+}) {    
+
+    const [feedback, setFeedback] = React.useState({
+        message: "",
+        severity: null as TSeverity,
+    });
+
+    return (
+        <FeedbackContext.Provider value={{ feedback, setFeedback }}>
+            {children}
+            <FeedbackSnackBar message={""} severity={null} />
+        </FeedbackContext.Provider>
+    );
+}
