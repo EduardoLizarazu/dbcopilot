a8d14bb02285cf8ab95a0babbd1b95167f441542
EduardoLizarazu <84466380+EduardoLizarazu@users.noreply.github.com>
2025-04-22 01:00:21 -0400

    Update chatFeedbackBtn.tsx

--- frontend-nextjs/src/components/chat/prompt/chatFeedbackBtn.tsx
+++ frontend-nextjs/src/components/chat/prompt/chatFeedbackBtn.tsx
@@ -1,4 +1,4 @@
-import { Container, IconButton, Stack } from "@mui/material";
+import { Container, IconButton, Stack, Tooltip } from "@mui/material";
 import ThumbUpOffAltIcon from "@mui/icons-material/ThumbUpOffAlt";
 import ThumbDownOffAltIcon from "@mui/icons-material/ThumbDownOffAlt";
 export function ChatFeedbackBtn() {
@@ -6,13 +6,16 @@ export function ChatFeedbackBtn() {
     <>
       <Container sx={{ display: "flex", justifyContent: "flex-end" }}>
         <Stack direction="row" spacing={0.5}>
-          <IconButton onClick={() => {}}>
-            <ThumbUpOffAltIcon />
-          </IconButton>
-
-          <IconButton onClick={() => {}}>
-            <ThumbDownOffAltIcon />
-          </IconButton>
+          <Tooltip title="Like" arrow placement="bottom">
+            <IconButton onClick={() => {}}>
+              <ThumbUpOffAltIcon />
+            </IconButton>
+          </Tooltip>
+          <Tooltip title="Dislike" arrow placement="bottom">
+            <IconButton onClick={() => {}}>
+              <ThumbDownOffAltIcon />
+            </IconButton>
+          </Tooltip>
         </Stack>
       </Container>
     </>
