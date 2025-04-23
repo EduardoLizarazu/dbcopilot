7029a8e559a2d9788f418010d1aac81b9f71900b
EduardoLizarazu <84466380+EduardoLizarazu@users.noreply.github.com>
2025-04-22 23:05:33 -0400

    feedback dislike dialog

--- frontend-nextjs/src/components/chat/prompt/chatFeedbackBtn.tsx
+++ frontend-nextjs/src/components/chat/prompt/chatFeedbackBtn.tsx
@@ -1,22 +1,44 @@
+"use client";
 import { Container, IconButton, Stack, Tooltip } from "@mui/material";
 import ThumbUpOffAltIcon from "@mui/icons-material/ThumbUpOffAlt";
 import ThumbDownOffAltIcon from "@mui/icons-material/ThumbDownOffAlt";
+import { ChatFeedbackDislikeDialog } from "./chatFeedbackDislikeDialog";
+import React from "react";
+
 export function ChatFeedbackBtn() {
+  const [disLikeData, setDisLikeData] = React.useState({
+    open: false,
+    feedback: "",
+  });
+
+  function handleLike() {
+    console.log("Like button clicked");
+  }
+  function handleDisLikeDialog() {
+    setDisLikeData((prev) => ({ ...prev, open: !prev.open }));
+  }
+
   return (
     <>
       <Container sx={{ display: "flex", justifyContent: "flex-end" }}>
         <Stack direction="row" spacing={0.5}>
           <Tooltip title="Like" arrow placement="bottom">
-            <IconButton onClick={() => {}}>
+            <IconButton onClick={handleLike}>
               <ThumbUpOffAltIcon />
             </IconButton>
           </Tooltip>
           <Tooltip title="Dislike" arrow placement="bottom">
-            <IconButton onClick={() => {}}>
+            <IconButton onClick={handleDisLikeDialog}>
               <ThumbDownOffAltIcon />
             </IconButton>
           </Tooltip>
         </Stack>
+        <ChatFeedbackDislikeDialog
+          open={disLikeData.open}
+          onClose={handleDisLikeDialog}
+          feedbackText={disLikeData.feedback}
+          setDisLikeData={setDisLikeData}
+        />
       </Container>
     </>
   );
---
+++ frontend-nextjs/src/components/chat/prompt/chatFeedbackDislikeDialog.tsx
@@ -0,0 +1,89 @@
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+
+type TChatFeedbackDislikeDialogProps = {
+  open: boolean;
+  onClose: () => void;
+  feedbackText: string;
+  setDisLikeData: React.Dispatch<
+    React.SetStateAction<{ open: boolean; feedback: string }>
+  >;
+};
+
+export function ChatFeedbackDislikeDialog({
+  open,
+  onClose,
+  feedbackText,
+  setDisLikeData,
+}: TChatFeedbackDislikeDialogProps) {
+  function handleFeedbackText(e: React.ChangeEvent<HTMLInputElement>) {
+    setDisLikeData((prev) => ({
+      ...prev,
+      feedback: e.target.value,
+    }));
+  }
+
+  return (
+    <>
+      <Dialog
+        open={open}
+        onClose={onClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">Feedback</DialogTitle>
+        <DialogContent>
+          <Typography variant="body2" color="text.secondary" gutterBottom>
+            We appreciate your feedback. Please let us know how we can improve
+            your experience with our service.
+          </Typography>
+          <TextField
+            value={feedbackText}
+            onChange={handleFeedbackText}
+            variant="outlined"
+            autoFocus
+            margin="dense"
+            id="name"
+            name="feedback"
+            label="Feedback"
+            type="text"
+            fullWidth
+            multiline
+            rows={6}
+            placeholder="Please provide your feedback here..."
+          />
+        </DialogContent>
+        <DialogActions>
+          <Stack
+            direction="row"
+            spacing={2}
+            justifyContent={"flex-end"}
+            alignItems={"center"}
+            sx={{ marginRight: 2, paddingBottom: 2 }}
+          >
+            <Button onClick={onClose} color="error" variant="outlined">
+              Cancel
+            </Button>
+            <Button
+              onClick={() => {}}
+              color="primary"
+              autoFocus
+              variant="contained"
+            >
+              Submit
+            </Button>
+          </Stack>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+}
