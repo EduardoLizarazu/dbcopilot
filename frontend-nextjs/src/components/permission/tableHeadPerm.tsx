7b7ab2370b2d92d44d9e8e76f4949ce3751466eb
EduardoLizarazu <84466380+EduardoLizarazu@users.noreply.github.com>
2025-06-24 11:27:49 -0400

    Update create permission link path
    Changed the 'Create' button link from '/connection/create' to 'auth/permission/create' in the TableHeadPerm component to reflect the correct route for creating permissions.

--- frontend-nextjs/src/components/permission/tableHeadPerm.tsx
+++ frontend-nextjs/src/components/permission/tableHeadPerm.tsx
@@ -49,7 +49,7 @@ export function TableHeadPerm({
           onChange={(e) => setSearchTerm(e.target.value)}
           sx={{ margin: 2, width: "300px" }}
         />
-        <Link href="/connection/create">
+        <Link href="auth/permission/create">
           <Button variant="contained" color="primary">
             Create
           </Button>
