15870b48bf9edabca70499d6fc576c9da7f33635
EduardoLizarazu <84466380+EduardoLizarazu@users.noreply.github.com>
2025-06-27 16:35:03 -0400

    Integrate Neo4j with SchemaGraphService
    Added Neo4jModule import to SchemaGraphModule and injected Neo4jService into SchemaGraphService. Updated the findAll method to query the Neo4j database for tables and columns, returning their details. Added error handling for database operations.

--- backend/src/auth/schema-graph/schema-graph.module.ts
+++ backend/src/auth/schema-graph/schema-graph.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SchemaGraphService } from './schema-graph.service';
 import { SchemaGraphController } from './schema-graph.controller';
+import { Neo4jModule } from 'src/neo4j/neo4j/neo4j.module';
 
 @Module({
+  imports: [Neo4jModule], // Add this line
   controllers: [SchemaGraphController],
   providers: [SchemaGraphService],
 })
--- backend/src/auth/schema-graph/schema-graph.service.ts
+++ backend/src/auth/schema-graph/schema-graph.service.ts
@@ -1,15 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateSchemaGraphDto } from './dto/create-schema-graph.dto';
 import { UpdateSchemaGraphDto } from './dto/update-schema-graph.dto';
+import { Neo4jService } from 'src/neo4j/neo4j/neo4j.service';
+import { BadRequestError } from 'openai';
 
 @Injectable()
 export class SchemaGraphService {
+  constructor(private readonly neo4jService: Neo4jService) {}
+
   create(createSchemaGraphDto: CreateSchemaGraphDto) {
     return 'This action adds a new schemaGraph';
   }
 
-  findAll() {
-    return `This action returns all schemaGraph`;
+  async findAll() {
+    try {
+      const cypher = `MATCH (table:Table)-[:HAS_COLUMN]->(column:Column)
+        RETURN
+          id(table) AS table_neo4j_id,     
+          table.name AS table_name,
+          table.alias AS table_alias,
+          table.description AS table_description,
+          id(column) AS column_neo4j_id,
+          column.name AS column_name,
+          column.alias AS column_alias,
+          column.description AS column_description,
+          column.key_type AS column_key_type,
+          column.type AS column_type
+      `;
+      const result = await this.neo4jService.read(cypher);
+      return result;
+    } catch (error) {
+      console.error(`Error on finding all graph: ${error}`);
+      throw new BadRequestException('Error on finding all graph');
+    }
   }
 
   findOne(id: number) {
