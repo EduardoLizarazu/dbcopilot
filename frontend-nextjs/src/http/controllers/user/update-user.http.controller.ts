3e90b2a325d17b4945bb151875abe7f315c97f1f
EduardoLizarazu <84466380+EduardoLizarazu@users.noreply.github.com>
2025-09-26 18:20:07 -0400

    Refactor UpdateUserController with enhanced auth and logging
    Refactored UpdateUserController to include detailed logging, improved error handling, and explicit authorization checks using roles. The controller now decodes JWT tokens, verifies admin role, and provides clearer responses and error messages. Dependencies and method signatures were updated for better type safety and maintainability.

--- frontend-nextjs/src/http/controllers/user/update-user.http.controller.ts
+++ frontend-nextjs/src/http/controllers/user/update-user.http.controller.ts
@@ -1,55 +1,135 @@
-import { IUpdateUserAppUseCase } from "@/core/application/usecases/user/update-user.app.usecase";
+import { IUpdateUserUseCase } from "@/core/application/usecases/user/update-user.app.usecase";
 import { IController } from "../IController.http.controller";
 import { HttpErrors } from "@/http/helpers/HttpErrors.http";
 import { IHttpErrors } from "@/http/helpers/IHttpErrors.http";
 import { IHttpSuccess } from "@/http/helpers/IHttpSuccess.http";
 import { HttpSuccess } from "@/http/helpers/HttpSuccess.http";
 import { IHttpResponse } from "@/http/helpers/IHttResponse.http";
-import { HttpRequest } from "@/http/helpers/HttpRequest.http";
 import { HttpResponse } from "@/http/helpers/HttpResponse.http";
 import { TUpdateUserDto } from "@/core/application/dtos/user.app.dto";
+import { IHttpRequest } from "@/http/helpers/IHttpRequest.http";
+import { IDecodeTokenPort } from "@/core/application/ports/decode-token.port";
+import { IAuthorizationRepository } from "@/core/application/interfaces/auth/auth.app.inter";
+import { ILogger } from "@/core/application/interfaces/ilog.app.inter";
+import { ROLE } from "@/http/utils/role.enum";
 
 export class UpdateUserController implements IController {
   constructor(
-    private updateUserUseCase: IUpdateUserAppUseCase,
+    private readonly logger: ILogger,
+    private readonly updateUserUseCase: IUpdateUserUseCase,
+    private readonly decodeTokenAdapter: IDecodeTokenPort,
+    private readonly accessRepo: IAuthorizationRepository,
     private httpErrors: IHttpErrors = new HttpErrors(),
     private httpSuccess: IHttpSuccess = new HttpSuccess()
   ) {}
-  async handle(httpRequest: HttpRequest): Promise<IHttpResponse> {
-    let error: IHttpResponse | null = null;
-    let response: IHttpResponse | null = null;
+  async handle(
+    httpRequest: IHttpRequest<TUpdateUserDto>
+  ): Promise<IHttpResponse> {
     try {
+      // ==== INPUT OF REQUEST ====
+      this.logger.info("[UpdateUserController] Handling request", httpRequest);
+
+      //   ==== INPUT HEADERS ====
+      //   1. Check headers
+      const headers = httpRequest.header as Record<string, string>;
+      this.logger.info("[UpdateUserController] Headers:", headers);
+      //   2. Check authorization
+      const authHeader =
+        headers["Authorization"] || headers["authorization"] || "";
+      if (!authHeader.startsWith("Bearer ")) {
+        this.logger.error(
+          "[UpdateUserController] Authorization token is missing",
+          httpRequest
+        );
+        const error = this.httpErrors.error_400(
+          "Authorization token is missing"
+        );
+        return new HttpResponse(error.statusCode, error.body);
+      }
+      const token = authHeader.replace("Bearer ", "");
+      this.logger.info("[UpdateUserController] Token:", token);
+
+      //   3. Decode token
+      const decoded = await this.decodeTokenAdapter.decodeToken(token);
+      if (!decoded) {
+        this.logger.error(
+          "[UpdateUserController] Invalid authorization token",
+          httpRequest
+        );
+        const error = this.httpErrors.error_401("Invalid authorization token");
+        return new HttpResponse(error.statusCode, error.body);
+      }
+      //   4. Retrieve roles
+      const roleNames = await this.accessRepo.findRolesNamesByUserId(
+        decoded.uid
+      );
+      this.logger.info(
+        "[UpdateUserController] User roles names:",
+        roleNames.roleNames
+      );
+      //   5. Check roles permissions
+      const { hasAuth } = await this.accessRepo.hasRoles({
+        ctxRoleNames: roleNames.roleNames,
+        requiredRoleNames: [ROLE.ADMIN],
+      });
+
+      if (!hasAuth) {
+        this.logger.error(
+          "[UpdateUserController] User does not have the required roles",
+          httpRequest
+        );
+        const error = this.httpErrors.error_401(
+          "User does not have the required roles"
+        );
+        return new HttpResponse(error.statusCode, error.body);
+      }
+
+      //   ==== INPUT BODY ====
       if (!httpRequest.body) {
-        error = this.httpErrors.error_400();
-        return new HttpResponse(error.statusCode, error.body);
-      }
-      const { body, path, query } = httpRequest;
-      const bodyParams = Object.keys(body);
-      const requiredParams = ["id", "email", "name", "roles"];
-      for (const param of requiredParams) {
-        if (!bodyParams.includes(param)) {
-          error = this.httpErrors.error_400();
-          return new HttpResponse(error.statusCode, error.body);
-        }
-      }
-
-      if (!path || !(path as { id?: string }).id) {
-        error = this.httpErrors.error_400();
+        this.logger.error(
+          "[UpdateUserController] No body provided",
+          httpRequest
+        );
+        const error = this.httpErrors.error_400("No body provided");
         return new HttpResponse(error.statusCode, error.body);
       }
 
-      const id = (path as { id: string }).id;
-      const updateUserDto = body as TUpdateUserDto;
-      const user = await this.updateUserUseCase.execute(id!, updateUserDto);
+      this.logger.info(
+        "[UpdateUserController] Received body:",
+        httpRequest.body
+      );
+
+      const body = httpRequest.body;
+
+      // ==== BUSINESS LOGIC ==== USE CASES ====
+      const user = await this.updateUserUseCase.execute(body.id, {
+        id: body.id,
+        email: body.email,
+        password: body.password,
+        lastname: body.lastname,
+        name: body.name,
+        roles: body.roles,
+      });
+
       if (!user.success) {
-        error = this.httpErrors.error_400();
+        this.logger.error("[UpdateUserController] User update failed:", user);
+        const error = this.httpErrors.error_400("User update failed");
         return new HttpResponse(error.statusCode, error.body);
       }
-      response = this.httpSuccess.success_200(user);
+
+      this.logger.info(
+        "[UpdateUserController] User updated successfully:",
+        user
+      );
+
+      const response = this.httpSuccess.success_200({
+        message: "User updated successfully",
+        data: user,
+      });
       return new HttpResponse(response.statusCode, response.body);
     } catch (err) {
-      console.error(err);
-      error = this.httpErrors.error_500();
+      this.logger.error("[UpdateUserController] Internal server error:", err);
+      const error = this.httpErrors.error_500("Internal server error");
       return new HttpResponse(error.statusCode, error.body);
     }
   }
