b6cf429fa3426ecc5013b13c0250a69b6dac8702
EduardoLizarazu <84466380+EduardoLizarazu@users.noreply.github.com>
2025-10-05 11:41:05 -0400

    Create read-all-vbd-splitter-composer.infra.service.ts

---
+++ frontend-nextjs/src/infrastructure/services/composers/vbd-splitter/read-all-vbd-splitter-composer.infra.service.ts
@@ -0,0 +1,46 @@
+import { ReadAllVbdSplitterUseCase } from "@/core/application/usecases/vbd-splitter/read-all-vbd-splitter.usecase";
+import { IController } from "@/http/controllers/IController.http.controller";
+import { ReadAllVbdSplitterController } from "@/http/controllers/vbd-splitter/read-all-vbd-splitter.http.controller";
+import { DecodeTokenAdapter } from "@/infrastructure/adapters/decode-token.adapter";
+import { FirebaseAdminProvider } from "@/infrastructure/providers/firebase/firebase-admin";
+import { WinstonLoggerProvider } from "@/infrastructure/providers/logging/winstom-logger.infra.provider";
+import { AuthorizationRepository } from "@/infrastructure/repository/auth.repo";
+import { VbdSplitterRepository } from "@/infrastructure/repository/vbd-splitter.repo";
+
+export function ReadAllVbdSplitterComposer(): IController {
+  // PROVIDERS
+  const loggerProvider = new WinstonLoggerProvider();
+  const firebaseAdmin = new FirebaseAdminProvider();
+
+  // REPOSITORIES
+  const vbdSplitterRepo = new VbdSplitterRepository(
+    loggerProvider,
+    firebaseAdmin
+  );
+
+  // Others utils
+  const decodeTokenAdapter = new DecodeTokenAdapter(
+    loggerProvider,
+    firebaseAdmin
+  );
+
+  const authRepository = new AuthorizationRepository(
+    loggerProvider,
+    firebaseAdmin
+  );
+
+  // USE CASES
+  const useCase = new ReadAllVbdSplitterUseCase(
+    loggerProvider,
+    vbdSplitterRepo
+  );
+
+  // CONTROLLER
+  const controller = new ReadAllVbdSplitterController(
+    loggerProvider,
+    useCase,
+    decodeTokenAdapter,
+    authRepository
+  );
+  return controller;
+}
