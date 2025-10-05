b26833ca434e2972d2804e5bf3bd5c414db46bf1
EduardoLizarazu <84466380+EduardoLizarazu@users.noreply.github.com>
2025-10-05 11:11:34 -0400

    Add DbConnectionRepository implementation
    Implements the DbConnectionRepository class for managing database connections using Firebase Admin SDK. Provides CRUD operations and methods to fetch connections with associated VBD Splitter data, supporting various query patterns and error logging.

---
+++ frontend-nextjs/src/infrastructure/repository/dbconnection.repo.ts
@@ -0,0 +1,306 @@
+import { ILogger } from "@/core/application/interfaces/ilog.app.inter";
+import { FirebaseAdminProvider } from "../providers/firebase/firebase-admin";
+import { IDbConnectionRepository } from "@/core/application/interfaces/dbconnection.inter";
+import {
+  TDbConnectionOutRequestDto,
+  TCreateDbConnectionDto,
+  TDbConnectionOutRequestDtoWithVbd,
+  TUpdateDbConnectionDto,
+} from "@/core/application/dtos/dbconnection.dto";
+import { TVbdOutRequestDto } from "@/core/application/dtos/vbd.dto";
+
+export class DbConnectionRepository implements IDbConnectionRepository {
+  constructor(
+    private readonly logger: ILogger,
+    private firebaseAdmin: FirebaseAdminProvider
+  ) {}
+  async create(data: TCreateDbConnectionDto): Promise<string> {
+    try {
+      this.logger.info("Creating DB Connection:", { ...data });
+
+      // Use Firebase Admin SDK to insert DB Connection
+      const db = this.firebaseAdmin.db;
+      const dbConnDocRef = await db
+        .collection(this.firebaseAdmin.coll.DB_CONNECTIONS)
+        .add(data);
+
+      await dbConnDocRef.update({ id: dbConnDocRef.id });
+
+      this.logger.info("Created DB Connection:", {
+        id: dbConnDocRef.id,
+        ...data,
+      });
+
+      return dbConnDocRef.id;
+    } catch (error) {
+      const errorMessage =
+        error.errorMessage || error.message || JSON.stringify(error);
+      this.logger.error("Failed to create DB Connection:", {
+        error: errorMessage,
+      });
+      throw new Error(`Failed to create DB Connection: ${errorMessage}`);
+    }
+  }
+  async update(id: string, data: TUpdateDbConnectionDto): Promise<void> {
+    try {
+      this.logger.info("Updating DB Connection:", { id, ...data });
+      // Use Firebase Admin SDK to update DB Connection
+      const db = this.firebaseAdmin.db;
+      await db
+        .collection(this.firebaseAdmin.coll.DB_CONNECTIONS)
+        .doc(id)
+        .update(data);
+      this.logger.info("Updated DB Connection:", { id, ...data });
+      return;
+    } catch (error) {
+      const errorMessage =
+        error.errorMessage || error.message || JSON.stringify(error);
+      this.logger.error("Failed to update DB Connection:", {
+        error: errorMessage,
+      });
+      throw new Error(`Failed to update DB Connection: ${errorMessage}`);
+    }
+  }
+  async delete(id: string): Promise<void> {
+    try {
+      this.logger.info("Deleting DB Connection:", { id });
+      // Use Firebase Admin SDK to delete DB Connection
+      const db = this.firebaseAdmin.db;
+      await db
+        .collection(this.firebaseAdmin.coll.DB_CONNECTIONS)
+        .doc(id)
+        .delete();
+      this.logger.info("Deleted DB Connection:", { id });
+      return;
+    } catch (error) {
+      const errorMessage =
+        error.errorMessage || error.message || JSON.stringify(error);
+      this.logger.error("Failed to delete DB Connection:", {
+        error: errorMessage,
+      });
+      throw new Error(`Failed to delete DB Connection: ${errorMessage}`);
+    }
+  }
+  async findByFields(
+    data: Partial<TCreateDbConnectionDto>
+  ): Promise<TDbConnectionOutRequestDto | null> {
+    try {
+      const db = this.firebaseAdmin.db;
+      const snapshotQuery = db
+        .collection(this.firebaseAdmin.coll.DB_CONNECTIONS)
+        .where("type", "==", data.type)
+        .where("host", "==", data.host)
+        .where("port", "==", data.port)
+        .where("database", "==", data.database)
+        .where("username", "==", data.username);
+
+      if (data.sid) {
+        snapshotQuery.where("sid", "==", data.sid);
+      }
+
+      const snapshot = await snapshotQuery.get();
+
+      if (snapshot.empty) {
+        this.logger.info("No DB Connection found by fields:", { ...data });
+        return null;
+      }
+
+      const doc = snapshot.docs[0];
+      const result = {
+        id: doc.id,
+        ...doc.data(),
+      } as TDbConnectionOutRequestDto;
+      this.logger.info("Found DB Connection by fields:", { ...data, result });
+      return result;
+    } catch (error) {
+      const errorMessage =
+        error.errorMessage || error.message || JSON.stringify(error);
+      this.logger.error("Failed to find DB Connection by fields:", {
+        error: errorMessage,
+      });
+      throw new Error(
+        `Failed to find DB Connection by fields: ${errorMessage}`
+      );
+    }
+  }
+  async findAllWithVbd(): Promise<TDbConnectionOutRequestDtoWithVbd[]> {
+    try {
+      this.logger.info("Finding all DB Connections with VBD");
+
+      // 1. Get all db connections
+      const dbSnapshot = await this.firebaseAdmin.db
+        .collection(this.firebaseAdmin.coll.DB_CONNECTIONS)
+        .get();
+      const dbConnections: TDbConnectionOutRequestDtoWithVbd[] = [];
+
+      dbSnapshot.forEach((doc) => {
+        const data = doc.data() as TDbConnectionOutRequestDto;
+        dbConnections.push({
+          id: doc.id,
+          ...data,
+          vbd_splitter: undefined,
+        });
+      });
+
+      this.logger.info(`Found ${dbConnections.length} DB Connections`, {
+        dbConnections,
+      });
+
+      // 2. For each db connection, get associated VBD Splitter on id_vbd_splitter
+
+      const vbdIds = dbConnections
+        .map((dbConn) => dbConn.id_vbd_splitter)
+        .filter((id): id is string => !!id);
+
+      this.logger.info(`Fetching VBD Splitters for IDs: ${vbdIds}`, { vbdIds });
+
+      const vbdMap: Record<string, TVbdOutRequestDto> = {};
+
+      if (vbdIds.length > 0) {
+        const vbdSnapshot = await this.firebaseAdmin.db
+          .collection(this.firebaseAdmin.coll.VBD_SPLITTERS)
+          .where("id", "in", vbdIds)
+          .get();
+
+        vbdSnapshot.forEach((doc) => {
+          const data = doc.data() as TVbdOutRequestDto;
+          vbdMap[doc.id] = data;
+        });
+      }
+      this.logger.info(`Fetched ${Object.keys(vbdMap).length} VBD Splitters`, {
+        vbdMap,
+      });
+      // 3. Combine data adding vbd_splitter fields containing vbd_splitter info
+      dbConnections.forEach((dbConn) => {
+        dbConn.vbd_splitter = vbdMap[dbConn.id_vbd_splitter];
+      });
+
+      this.logger.info("Combined DB Connections with VBD Splitters", {
+        dbConnections,
+      });
+
+      return dbConnections;
+    } catch (error) {
+      const errorMessage =
+        error.errorMessage || error.message || JSON.stringify(error);
+      this.logger.error("Failed to find all DB Connections with VBD:", {
+        error: errorMessage,
+      });
+      throw new Error(
+        `Failed to find all DB Connections with VBD: ${errorMessage}`
+      );
+    }
+  }
+  async findWithVbdById(
+    id: string
+  ): Promise<TDbConnectionOutRequestDtoWithVbd | null> {
+    try {
+      // 1. Get DB connection by ID
+      const dbConnDoc = this.firebaseAdmin.db
+        .collection(this.firebaseAdmin.coll.DB_CONNECTIONS)
+        .doc(id);
+      const dbConnData = await dbConnDoc.get();
+      if (!dbConnData.exists) {
+        return null;
+      }
+
+      // 2. Get associated VBD Splitter on id_vbd_splitter
+      const dbConn = {
+        id: dbConnData.id,
+        ...dbConnData.data(),
+        vbd_splitter: undefined,
+      } as TDbConnectionOutRequestDtoWithVbd;
+
+      if (dbConn.id_vbd_splitter) {
+        const vbdDoc = this.firebaseAdmin.db
+          .collection(this.firebaseAdmin.coll.VBD_SPLITTERS)
+          .doc(dbConn.id_vbd_splitter);
+        const vbdData = await vbdDoc.get();
+        if (vbdData.exists) {
+          dbConn.vbd_splitter = {
+            id: vbdData.id,
+            ...vbdData.data(),
+          } as TVbdOutRequestDto;
+        }
+      }
+
+      return dbConn;
+    } catch (error) {
+      const errorMessage =
+        error.errorMessage || error.message || JSON.stringify(error);
+      this.logger.error("Failed to find DB Connection with VBD by ID:", {
+        error: errorMessage,
+      });
+      throw new Error(
+        `Failed to find DB Connection with VBD by ID: ${errorMessage}`
+      );
+    }
+  }
+  async findById(id: string): Promise<TDbConnectionOutRequestDto | null> {
+    try {
+      // Use Firebase Admin SDK to fetch DB Connection by ID
+      const db = this.firebaseAdmin.db;
+      const dbConnDoc = await db
+        .collection(this.firebaseAdmin.coll.DB_CONNECTIONS)
+        .doc(id)
+        .get();
+      if (!dbConnDoc.exists) {
+        return null;
+      }
+      return {
+        id: dbConnDoc.id,
+        ...dbConnDoc.data(),
+      } as TDbConnectionOutRequestDto;
+    } catch (error) {
+      const errorMessage =
+        error.errorMessage || error.message || JSON.stringify(error);
+      this.logger.error("Failed to find DB Connection by ID:", {
+        error: errorMessage,
+      });
+      throw new Error(`Failed to find DB Connection by ID: ${errorMessage}`);
+    }
+  }
+  async findAll(): Promise<TDbConnectionOutRequestDto[]> {
+    try {
+      const db = this.firebaseAdmin.db;
+      const dbConnDocs = await db
+        .collection(this.firebaseAdmin.coll.DB_CONNECTIONS)
+        .get();
+      return dbConnDocs.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      })) as TDbConnectionOutRequestDto[];
+    } catch (error) {
+      const errorMessage =
+        error.errorMessage || error.message || JSON.stringify(error);
+      this.logger.error("Failed to find all DB Connections:", {
+        error: errorMessage,
+      });
+      throw new Error(`Failed to find all DB Connections: ${errorMessage}`);
+    }
+  }
+  async findByName(name: string): Promise<TDbConnectionOutRequestDto | null> {
+    try {
+      const db = this.firebaseAdmin.db;
+      const dbConnDocs = await db
+        .collection(this.firebaseAdmin.coll.DB_CONNECTIONS)
+        .where("name", "==", name)
+        .get();
+      if (dbConnDocs.empty) {
+        return null;
+      }
+      const doc = dbConnDocs.docs[0];
+      return {
+        id: doc.id,
+        ...doc.data(),
+      } as TDbConnectionOutRequestDto;
+    } catch (error) {
+      const errorMessage =
+        error.errorMessage || error.message || JSON.stringify(error);
+      this.logger.error("Failed to find DB Connection by name:", {
+        error: errorMessage,
+      });
+      throw new Error(`Failed to find DB Connection by name: ${errorMessage}`);
+    }
+  }
+}
