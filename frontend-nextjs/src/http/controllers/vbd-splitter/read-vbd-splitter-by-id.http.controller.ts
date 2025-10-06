1dab2daffe51ecac27251c19ee447285e64ce51c
EduardoLizarazu <84466380+EduardoLizarazu@users.noreply.github.com>
2025-10-06 09:58:40 -0400

    Create read-vbd-splitter-by-id.http.controller.ts

---
+++ frontend-nextjs/src/http/controllers/vbd-splitter/read-vbd-splitter-by-id.http.controller.ts
@@ -0,0 +1,134 @@
+import { ILogger } from "@/core/application/interfaces/ilog.app.inter";
+import { IController } from "../IController.http.controller";
+import { IDecodeTokenPort } from "@/core/application/ports/decode-token.port";
+import { IAuthorizationRepository } from "@/core/application/interfaces/auth/auth.app.inter";
+import { HttpErrors } from "@/http/helpers/HttpErrors.http";
+import { IHttpErrors } from "@/http/helpers/IHttpErrors.http";
+import { IHttpSuccess } from "@/http/helpers/IHttpSuccess.http";
+import { HttpSuccess } from "@/http/helpers/HttpSuccess.http";
+import { IHttpRequest } from "@/http/helpers/IHttpRequest.http";
+import { IHttpResponse } from "@/http/helpers/IHttResponse.http";
+import { HttpResponse } from "@/http/helpers/HttpResponse.http";
+import { ROLE } from "@/http/utils/role.enum";
+import { IReadNlqQaGoodByIdUseCase } from "@/core/application/usecases/nlq/nlq-qa-good/read-nlq-qa-good-by-id.usecase";
+import { IReadVbdSplitterByIdUseCase } from "@/core/application/usecases/vbd-splitter/read-vbd-splitter-by-id.usecase";
+
+export class ReadVbdSplitterByIdController implements IController {
+  constructor(
+    private readonly logger: ILogger,
+    private readonly readVbdSplitterByIdUseCase: IReadVbdSplitterByIdUseCase,
+    private readonly decodeTokenAdapter: IDecodeTokenPort,
+    private readonly accessRepo: IAuthorizationRepository,
+    private httpErrors: IHttpErrors = new HttpErrors(),
+    private httpSuccess: IHttpSuccess = new HttpSuccess()
+  ) {}
+
+  async handle(httpRequest: IHttpRequest<null>): Promise<IHttpResponse> {
+    try {
+      // ==== INPUT OF REQUEST ====
+      this.logger.info(
+        "[ReadVbdSplitterByIdController] handling request",
+        httpRequest
+      );
+
+      //   ==== INPUT HEADERS ====
+      //   1. Check headers
+      const headers = httpRequest.header as Record<string, string>;
+      this.logger.info("[ReadVbdSplitterByIdController] Headers:", headers);
+      //   2. Check authorization
+      const authHeader =
+        headers["Authorization"] || headers["authorization"] || "";
+      if (!authHeader.startsWith("Bearer ")) {
+        this.logger.error(
+          "[ReadVbdSplitterByIdController] No token provided",
+          httpRequest
+        );
+        const error = this.httpErrors.error_400(
+          "Error retrieving VBD Splitter"
+        );
+        return new HttpResponse(error.statusCode, error.body);
+      }
+      const token = authHeader.replace("Bearer ", "");
+      this.logger.info("[ReadVbdSplitterByIdController] Token:", token);
+
+      //   3. Decode token
+      const decoded = await this.decodeTokenAdapter.decodeToken(token);
+      if (!decoded) {
+        this.logger.error(
+          "[ReadVbdSplitterByIdController] Invalid token",
+          httpRequest
+        );
+        const error = this.httpErrors.error_401("Invalid token");
+        return new HttpResponse(error.statusCode, error.body);
+      }
+      //   4. Retrieve roles
+      const roleNames = await this.accessRepo.findRolesNamesByUserId(
+        decoded.uid
+      );
+      this.logger.info(
+        "[ReadVbdSplitterByIdController] User roles names:",
+        roleNames.roleNames
+      );
+      //   5. Check roles permissions
+      const { hasAuth } = await this.accessRepo.hasRoles({
+        ctxRoleNames: roleNames.roleNames,
+        requiredRoleNames: [],
+      });
+      if (!hasAuth) {
+        this.logger.error(
+          "[ReadVbdSplitterByIdController] User is not authorized"
+        );
+        const error = this.httpErrors.error_401("User is not authorized");
+        return new HttpResponse(error.statusCode, error.body);
+      }
+
+      //   ==== INPUT PARAMS ====
+      if (!httpRequest.params || !httpRequest.params.id) {
+        this.logger.error(
+          "[ReadVbdSplitterByIdController] No id parameter provided",
+          httpRequest
+        );
+        const error = this.httpErrors.error_400("No id parameter provided");
+        return new HttpResponse(error.statusCode, error.body);
+      }
+
+      this.logger.info(
+        "[ReadVbdSplitterByIdController] Received params:",
+        httpRequest.params
+      );
+
+      // ==== BUSINESS LOGIC USE CASES ====
+      const useCase = await this.readVbdSplitterByIdUseCase.execute(
+        httpRequest.params.id
+      );
+      this.logger.info(
+        "[ReadVbdSplitterByIdController] UseCase executed successfully",
+        useCase
+      );
+
+      if (!useCase.success) {
+        this.logger.error(
+          "[ReadVbdSplitterByIdController] Error retrieving VBD Splitter",
+          {
+            ...useCase,
+          }
+        );
+        const error = this.httpErrors.error_400(
+          "Error retrieving VBD Splitter: " + useCase.message
+        );
+        return new HttpResponse(error.statusCode, error.body);
+      }
+
+      // ==== OUTPUT OF RESPONSE ====
+      const success = this.httpSuccess.success_200({
+        message: useCase.message,
+        data: useCase.data,
+      });
+      return new HttpResponse(success.statusCode, success.body);
+    } catch (error) {
+      this.logger.error("[ReadVbdSplitterByIdController] Error:", error);
+      const httpError = this.httpErrors.error_500("Internal server error");
+      return new HttpResponse(httpError.statusCode, httpError.body);
+    }
+  }
+}
