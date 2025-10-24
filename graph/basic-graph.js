2350b5197bc2dddb98e5bbba9654148fd8e2927f
EduardoLizarazu <84466380+EduardoLizarazu@users.noreply.github.com>
2025-10-24 12:16:14 -0400

    Refactor graph demo logic into BasicGraphTest
    Moved the demonstration and testing code from main.js into a new BasicGraphTest function in basic-graph.js. main.js now simply calls BasicGraphTest, improving separation of concerns and making the graph class easier to test and reuse.

--- graph/basic-graph.js
+++ graph/basic-graph.js
@@ -117,4 +117,34 @@ class BasicGraph {
   }
 }
 
-module.exports = BasicGraph;
+function BasicGraphTest() {
+  // Create a graph and add nodes/edges (matches the original example)
+  const g = new BasicGraph();
+
+  // create 3 nodes with labels 0,1,2
+  g.addNode(0);
+  g.addNode(1);
+  g.addNode(2);
+
+  // Now add edges one by one
+  g.addEdge(1, 0, 4);
+  g.addEdge(1, 2, 3);
+  g.addEdge(2, 0, 1);
+
+  console.log("Adjacency List Representation:");
+  g.displayAdjList();
+
+  // Demonstrate removeEdge: remove edge 1 -> 2
+  console.log("\nRemoving edge 1 -> 2");
+  const removedCount = g.removeEdge(1, 2);
+  console.log(`removed edges: ${removedCount}`);
+  g.displayAdjList();
+
+  // Demonstrate removeNode: remove node labeled 0
+  console.log("\nRemoving node 0");
+  const removedLabel = g.removeNode(0);
+  console.log(`removed node label: ${removedLabel}`);
+  g.displayAdjList();
+}
+
+module.exports = BasicGraphTest;
--- graph/main.js
+++ graph/main.js
@@ -1,34 +1,8 @@
 // Function to add an edge between two vertices
-const BasicGraph = require("./basic-graph");
+const BasicGraphTest = require("./basic-graph");
 
 function main() {
-  // Create a graph and add nodes/edges (matches the original example)
-  const g = new BasicGraph();
-
-  // create 3 nodes with labels 0,1,2
-  g.addNode(0);
-  g.addNode(1);
-  g.addNode(2);
-
-  // Now add edges one by one
-  g.addEdge(1, 0, 4);
-  g.addEdge(1, 2, 3);
-  g.addEdge(2, 0, 1);
-
-  console.log("Adjacency List Representation:");
-  g.displayAdjList();
-
-  // Demonstrate removeEdge: remove edge 1 -> 2
-  console.log("\nRemoving edge 1 -> 2");
-  const removedCount = g.removeEdge(1, 2);
-  console.log(`removed edges: ${removedCount}`);
-  g.displayAdjList();
-
-  // Demonstrate removeNode: remove node labeled 0
-  console.log("\nRemoving node 0");
-  const removedLabel = g.removeNode(0);
-  console.log(`removed node label: ${removedLabel}`);
-  g.displayAdjList();
+  BasicGraphTest();
 }
 
 main();
