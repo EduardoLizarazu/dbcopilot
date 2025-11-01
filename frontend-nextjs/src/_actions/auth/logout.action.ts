565cb191cd0e96d70eb010038833cf8afac997de
EduardoLizarazu <84466380+EduardoLizarazu@users.noreply.github.com>
2025-11-01 14:21:20 -0400

    Update logout.action.ts

--- frontend-nextjs/src/_actions/auth/logout.action.ts
+++ frontend-nextjs/src/_actions/auth/logout.action.ts
@@ -1,16 +1,21 @@
 // controller/_actions/auth/logout.ts
 "use server";
 
+import { FirebaseClientProvider } from "@/infrastructure/providers/firebase/firebase-client";
+import { COOKIE_NAME } from "@/utils/constants";
+import { signOut } from "firebase/auth";
 import { cookies } from "next/headers";
 
 export async function logoutAction() {
   const cookieStore = await cookies();
-  cookieStore.set("fb_id_token", "", {
+  cookieStore.set(COOKIE_NAME, "", {
     httpOnly: true,
     secure: true,
     sameSite: "lax",
     path: "/",
     maxAge: 0,
   });
+  const auth = new FirebaseClientProvider().getAuth();
+  await signOut(auth);
   return { ok: true };
 }
