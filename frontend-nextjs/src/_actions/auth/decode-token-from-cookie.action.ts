873508e00a88c25c9a72e4b04cc22707f3ba6b38
EduardoLizarazu <84466380+EduardoLizarazu@users.noreply.github.com>
2025-11-01 14:26:54 -0400

    Update decode-token-from-cookie.action.ts

--- frontend-nextjs/src/_actions/auth/decode-token-from-cookie.action.ts
+++ frontend-nextjs/src/_actions/auth/decode-token-from-cookie.action.ts
@@ -1,5 +1,6 @@
 import { cookies } from "next/headers";
 import { adminAuth } from "@/infrastructure/providers/firebase/firebase-admin"; // your Admin SDK init
+import { COOKIE_NAME } from "@/utils/constants";
 
 export type DecodedUser = {
   uid: string;
@@ -10,7 +11,7 @@ export type DecodedUser = {
 };
 
 export async function DecodeTokenFromCookieAction(): Promise<DecodedUser | null> {
-  const token = (await cookies()).get("fb_id_token")?.value;
+  const token = (await cookies()).get(COOKIE_NAME)?.value;
   if (!token) return null;
 
   try {
