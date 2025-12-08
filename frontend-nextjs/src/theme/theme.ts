e3e2f00405b3102e513359c3e26b49bd5ee9b78e
EduardoLizarazu <84466380+EduardoLizarazu@users.noreply.github.com>
2025-12-08 17:07:35 -0400

    Update primary and secondary color hex codes
    Changed the commented HEX codes for primary and secondary colors in the theme configuration to new values for improved color consistency.

--- frontend-nextjs/src/theme/theme.ts
+++ frontend-nextjs/src/theme/theme.ts
@@ -10,13 +10,13 @@ import { createTheme } from "@mui/material/styles";
 const theme = createTheme({
   palette: {
     // primary: {
-    //   main: "#f0601d", // <--- Pón aquí tu código HEX de color primario
+    //   main: "#e06933ff", // <--- Pón aquí tu código HEX de color primario
     //   // light: '#ff8a65', // Opcional: calculará uno automáticamente si no lo pones
     //   // dark: '#c41c00',  // Opcional
     //   // contrastText: '#ffffff', // Opcional: color del texto sobre el primario
     // },
     // secondary: {
-    //   main: "#19857b",
+    //   main: "#197d73ff",
     // },
   },
 });
